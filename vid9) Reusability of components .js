971b2c441701ce34cc2407da1d8025f3aa2b8615
Vaibhav_7221 <76476010+vaibhavsharma7221@users.noreply.github.com>
2022-01-13 18:10:21 +0530

    Create vid9) Reusability of components .js

---
+++ vid9) Reusability of components .js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+import Hello from './components/Hello';
+function App() {
+  return (  
+    <div className="App">
+      <Greet/>
+      <Greet/>
+      <Greet/>
+      {/*<Welcome/> 
+     <Hello/>*/}
+    </div>
+  );
+}
+
+export default App;
