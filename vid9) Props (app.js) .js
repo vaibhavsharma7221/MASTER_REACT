1d74c8c6494077274abeab71423d6b465e705d2d
Vaibhav_7221 <76476010+vaibhavsharma7221@users.noreply.github.com>
2022-01-14 11:28:06 +0530

    Create vid9) Props (app.js) .js

---
+++ vid9) Props (app.js) .js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+import Hello from './components/Hello';
+function App() {
+  return (  
+    <div className="App">
+      <Greet name="Bruce" charactername="batman">
+        this is children props
+      </Greet>
+      <Greet name="Clark" charactername="superman">
+        <button>action</button>
+      </Greet>
+      <Greet name="Diana" charactername="wonderwomen"/>
+      {/*<Welcome/> 
+     <Hello/>*/}
+    </div>
+  );
+}
+
+export default App;
