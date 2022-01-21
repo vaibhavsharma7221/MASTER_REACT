e5d9db150545a6860d138568c778fe079e7db52b
Vaibhav_7221 <76476010+vaibhavsharma7221@users.noreply.github.com>
2022-01-21 12:13:05 +0530

    Create vid12) Destructing props & state using functional components.js

---
+++ vid12) Destructing props & state using functional components.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+
+class Welcome extends Component{
+    render(){
+        const {name,heroName} = this.props
+        //const {state1 , state2} = this.state
+        return(
+        <h1>welcome {name} - {heroName}</h1>
+        )
+    }
+}
+
+export default Welcome
