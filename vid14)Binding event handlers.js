2b7eb3d629b3078371adba3082c55a5f0b06823c
Vaibhav_7221 <76476010+vaibhavsharma7221@users.noreply.github.com>
2022-01-22 18:22:23 +0530

    Create vid14)Binding event handlers.js

---
+++ vid14)Binding event handlers.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+ class EventBind extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             message:"hello"
+        }
+        // this.clickHandler=this.clickHandler.bind(this)
+    }
+
+    clickHandler = () => {
+        this.setState({
+            message:"goodbye!"
+        })
+    }
+    
+    // clickHandler(){
+    //     this.setState({
+    //          message:"goodbye!"
+    //     })
+    //     console.log(this)
+    // }
+
+    render() {
+        return (
+            <div>
+                <div>{this.state.message}</div>
+            {/* <button onClick={this.clickHandler.bind(this)}>click</button> */}
+            {/* <button onClick={()=>this.clickHandler()}>click</button> */}
+            <button onClick={this.clickHandler}>click</button>
+            </div>
+        )
+     }
+}
+
+export default EventBind
