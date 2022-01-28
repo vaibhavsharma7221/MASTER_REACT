35b3baad5abea551c17962ab07cd048a6ce17e12
Vaibhav_7221 <76476010+vaibhavsharma7221@users.noreply.github.com>
2022-01-28 12:39:47 +0530

    Create vid 18) Lists and Keys.js

---
+++ vid 18) Lists and Keys.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Person from './Person';
+
+function NameList() {
+
+    const persons = [
+        {
+            id: 1,
+            name: 'Bruce',
+            age: 30,
+            skill: 'React'
+        },
+
+        {
+            id: 2,
+            name: 'Clark',
+            skill: 'angular'
+        },
+
+        {
+            id: 3,
+            name: 'Diana',
+            skill: 'vue'
+        }
+
+    ]
+    const personList = persons.map(person =>
+        <Person key={person.id} person={person} />
+    )
+    return <div>{personList}</div>;
+}
+
+export default NameList;
